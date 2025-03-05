a5461e50e5d80afe5c7113da0eee3e05f3eb26d3
kolawoletech <kolawolegolulana@gmail.com>
2025-03-06 01:19:06 +0200

    feat (reactive form) : add reactive form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { CrudComponent } from './crud/crud.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 
 export const routes: Routes = [
     { path: 'crud', component: CrudComponent },
     { path: '', redirectTo: '/crud', pathMatch: 'full' },
-    { path: '**', redirectTo: '/crud' }
+    { path: '**', redirectTo: '/crud' },
+    { path: 'form', component: ReactiveFormComponent }
   ];
